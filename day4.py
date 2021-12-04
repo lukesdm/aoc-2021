8e538f0a93d0ac3683fae1a026d15be7fcc8d8f4
lukesdm <2824165+lukesdm@users.noreply.github.com>
2021-12-04 10:36:21 +0100

    Day 4 wip 2

--- day4.py
+++ day4.py
@@ -1,25 +1,66 @@
-import numpy as np
+from typing import Dict
+
+
+class Board:
+    def __init__(self, id, numbers: Dict):
+        self.id = id
+        self.numbers = numbers
+        self.colMarks = [0] * 5
+        self.rowMarks = [0] * 5
+
+def parseBoard(lines, boardId):
+    nums = {}
+    for row in range(0, len(lines)):
+        col = 0
+        for num in lines[row].strip().split():
+            nums[int(num)] = (row, col)
+            col = col + 1
+    return Board(boardId, nums)
 
 # First line of picks, then grids (5x5) separated by blank line.
 def parse(lines):
     picks = map(int, lines[0].split(","))
-    grids = []
+    boards = []
     # 1 grid every 6 lines starting at lines[2]
-    parseGrid = lambda l: list(map(int, l.strip().split()))
     end = len(lines) - 4
+    boardId = 1
     for s in range(2, end, 6):
-        grid = list(map(parseGrid, lines[s:s+5]))
-        npGrid = np.array(grid)
-        grids.append(npGrid)
-    return picks, grids
-    
+        grid = parseBoard(lines[s:s+5], boardId)
+        boards.append(grid)
+        boardId += 1
+    return picks, boards
+
+def has5(x):
+    return x == 5
+
+def checkBoard(board: Board, pick):
+    if pick in board.numbers:
+        row, col = board.numbers[pick]
+        board.rowMarks[row] += 1
+        board.colMarks[col] += 1
+    return any(filter(has5, board.rowMarks)) or any(filter(has5, board.colMarks))
+
+# def calcScore(board: Board):
+
+
 input = None
 filename = "day4-sample.txt" # "day4-input.txt" 
 with open(filename) as reader:
     input = reader.readlines()
 
-# formatter = lambda item: int(item.strip())
-# seq = map(formatter, input)
-picks, grids = parse(input)
+picks, boards = parse(input)
 
-print(grids[0][:,0])
+winningBoard = None
+
+for pick in picks:
+    for board in boards:
+        if checkBoard(board, pick):
+            print("Winner!")
+            winningBoard = board.id
+            break
+    if winningBoard != None:
+        break
+
+print(board.id)
+result = calcScore(board)
+print(result)
